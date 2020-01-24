884dbac10f226c825407650e21c2969789e30f6b
Lucas Sherwin <lksherwin@gmail.com>
2020-01-24 18:47:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,7 @@ def square_array(array)
   # your code here
   i = 0
   while i < array.length do
-    array[i] = array[i]**2
+    array[i] = (array[i])**2
     i += 1
-    puts array
   end
-  #puts array
 end
